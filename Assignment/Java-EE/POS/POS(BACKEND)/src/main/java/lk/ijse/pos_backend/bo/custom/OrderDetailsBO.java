45e4e2857f007e48dc655ba135fdf63556e68f05
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-02-01 20:23:58 +0530

    BOFactory class updated

--- Assignment/Java-EE/POS/POS(BACKEND)/src/main/java/lk/ijse/pos_backend/bo/custom/OrderDetailsBO.java
+++ Assignment/Java-EE/POS/POS(BACKEND)/src/main/java/lk/ijse/pos_backend/bo/custom/OrderDetailsBO.java
@@ -3,6 +3,10 @@ package lk.ijse.pos_backend.bo.custom;
 import lk.ijse.pos_backend.bo.SuperBO;
 import lk.ijse.pos_backend.dto.OrderDetailsDTO;
 
+/**
+*@author: Vishal Sandakelum,
+*@Runtime version: 11.0.11+9-b1341.60 amd64
+**/
 public interface OrderDetailsBO extends SuperBO {
     String SaveOrderDetails(OrderDetailsDTO orderDetailsDTO);
     OrderDetailsDTO getOrderDetails(String id);
