a86f8081984129d7a42116586fd6322dbe33825d
vishalgit2005 <vishalsandakelum20@gmail.com>
2024-02-06 10:28:02 +0530

    Initialize App Content Listner

--- Assignment/Java-EE/POS/POS(BACKEND)/src/main/java/lk/ijse/pos_backend/api/CustomerApi.java
+++ Assignment/Java-EE/POS/POS(BACKEND)/src/main/java/lk/ijse/pos_backend/api/CustomerApi.java
@@ -1,20 +1,42 @@
 package lk.ijse.pos_backend.api;
 
 import lk.ijse.pos_backend.config.SessionFactoryConfig;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.Session;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
 *@author: Vishal Sandakelum,
 *@Runtime version: 11.0.11+9-b1341.60 amd64
 **/
+
+@WebServlet(name = "Customer",urlPatterns = "/customer")
 public class CustomerApi extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-        System.out.println();
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource dataSource = (BasicDataSource) servletContext.getAttribute("DBCP");
+
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 }
